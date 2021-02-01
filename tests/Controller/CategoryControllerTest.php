7703d989845870c478fdfdbd364fd93a99156296
Kostya <pavlyukkonstantin85@gmail.com>
2021-02-01 05:57:04 +0300

    added create category test

--- tests/Controller/CategoryControllerTest.php
+++ tests/Controller/CategoryControllerTest.php
@@ -3,18 +3,32 @@
 
 namespace App\Tests\Controller;
 
-use App\DataFixtures\CategoryFixtures;
+
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Response;
+
 
 class CategoryControllerTest extends WebTestCase
 {
+
+    public function testCreateCategory()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/v3.0/category/create');
+        $client->submitForm('Submit', [
+            'category_form[name]' => 'Name1',
+            'comment_form[description]' => 'Description1',
+            ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();              //принудительно вызываем перенаправление
+        $this->assertSelectorExists('("")');
+    }
+
     public function testShowCategories()
     {
-        $client=static::createClient();
-        $client ->request('GET', '/v3.0/category/');
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $client = static::createClient();
+        $client->request('GET', '/v3.0/category');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
 }
